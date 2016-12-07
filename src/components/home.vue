206a3cbe4fc9085f0d22740b81f3cf519fcee300
18704006306@139.com <18704006306@163.com>
2016-12-07 15:26:22 +0800

    rays-ui v0.1.2
    文档编写

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,28 @@
+<style>
+	.homeBody{font-size: 14px; line-height: 2em; color: rgba(52,73,94,1);}
+	.homeBody p{padding-left: 2em;}
+	.homeBody h1,.homeBody h2,.homeBody h3,.homeBody h4,.homeBody h5{font-family: "Microsoft Yahei","微软雅黑";}
+</style>
 <template>
-  <div class="jumbotron">
-    <h1>任务追踪</h1>
-    <p>
-      <strong>
-        <a v-link="'/time-entries'">创建一个任务</a>
-      </strong>
-    </p>
-  </div>
+	<div class="homeBody">
+		<h2>RAYS-UI</h2>
+		<p>rays-ui是一个基于vue.js开发的组件库,目的是帮助你快速完成产品的开发</p>
+		<h3>特点</h3>
+		<ul>
+			<li>npm + webpack + vue + bootstrap 的开发模式</li>
+			<li>通过babel使用ES6语法进行开发使用</li>
+			<li>引用bootstrap响应式布局及部分样式</li>
+			<li>引用Font Awesome图标库</li>
+			<li>引用Echarts图表</li>
+			<li>Flat风格设计,兼顾了大数据量展示</li>
+			<li>推荐使用数据驱动的前端思想,根据需要,也可引用jQuery进行DOM操作</li>
+		</ul>
+		<h3>安装</h3>
+		<div>
+			<p>1.安装npm</p>
+			<p>2.通过github下载框架</p>
+			<p>3.运行npm install安装依赖文件</p>
+			<p>4.运行run npm dev运行框架</p>
+		</div>
+	</div>
 </template>
